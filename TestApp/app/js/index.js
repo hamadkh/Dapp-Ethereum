6063e2373d162f9f6206f46facd89ebb533980f6
hamad Khawaja <khawajahamad217@gmail.com>
2018-02-16 11:49:43 -0500

    delete

--- TestApp/app/js/index.js
+++
@@ -1,4 +0,0 @@
-# app/js/index.js
-SimpleStorage.set(100);
-SimpleStorage.get().then(function(value) { console.log(value.toNumber()) });
-SimpleStorage.storedData().then(function(value) { console.log(value.toNumber()) });
